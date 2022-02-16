2f182194370ff669f75fceb906fd0b92c03763b1
LukeMcCann <developer-lukemccann@outlook.com>
2022-02-16 22:18:07 +0000

    Add initial game state

--- app/src/game.mjs
+++ app/src/game.mjs
@@ -5,3 +5,53 @@ import kaboom from 'kaboom';
 kaboom();
 
 loadRoot("http://localhost:3001/");
+loadSprite('invader', 'images/sprites/invader.png');
+loadSprite('wall', 'images/sprites/wall.png');
+loadSprite('player', 'images/sprites/ship.png');
+
+camPos(300, 150);
+camScale(2.2);
+
+const PLAYER_VELOCITY = 200;
+
+const player = add([
+    sprite('player'),
+    scale(0.32),
+    pos(280, 200),
+    origin('center'),
+]);
+
+keyDown('right', () => {
+    player.move(PLAYER_VELOCITY, 0);
+});
+keyDown('left', () => {
+    player.move(-PLAYER_VELOCITY, 0);
+});
+
+addLevel([
+    '!^^^^^^^^^^^^^^^     &',
+    '!^^^^^^^^^^^^^^^     &',
+    '!^^^^^^^^^^^^^^^     &',
+    '!^^^^^^^^^^^^^^^     &',
+    '!                    &',
+    '!                    &',
+    '!                    &',
+    '!                    &',
+    '!                    &',
+    '!                    &',
+], {
+    width: 30, 
+    height: 22, 
+    '^': () => [
+        sprite('invader'),
+        scale(0.32)
+    ],
+    '!': () => [
+        sprite('wall'),
+        'left-wall',
+    ],
+    '&': () => [
+        sprite('wall'),
+        'right-wall',
+    ],
+});
